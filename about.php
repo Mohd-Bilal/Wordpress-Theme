450bd56b98bb924455ae639295ca1088e56ec0ce
Mohd-Bilal <bilaljaleel123@gmail.com>
2019-06-19 19:39:57 +0530

    Added overlay to main carousel

--- about.php
+++ about.php
@@ -6,21 +6,8 @@
         <img  src=<?php  echo get_template_directory_uri().'/images/Sreejesh.png' ?>  alt="">
         </div>
         <div class="col-lg-7 col-md-7 col-sm-12 col-xm-12 ">
-        <p> A  studio has always been my wish. But a community has always bee my dream. I wanted to make AuO2 Studio
-            a space where people can come, express, record, mix and above all be creative.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Quam, velit consequatur possimus unde maxime iusto delectus explicabo sit repudiandae minima, 
-            laudantium distinctio est corrupti reiciendis quae soluta nostrum aspernatur necessitatibus.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Quam, velit consequatur possimus unde maxime iusto delectus explicabo sit repudiandae minima, 
-            laudantium distinctio est corrupti reiciendis quae soluta nostrum aspernatur necessitatibus.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Quam, velit consequatur possimus unde maxime iusto delectus explicabo sit repudiandae minima, 
-            laudantium distinctio est corrupti reiciendis quae soluta nostrum aspernatur necessitatibus.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Quam, velit consequatur possimus unde maxime iusto delectus explicabo sit repudiandae minima, 
-            laudantium distinctio est corrupti reiciendis quae soluta nostrum aspernatur necessitatibus.
-            <br><br>
+        <p>" A  studio has always been my wish. But a community has always bee my dream. I wanted to make AuO2 Studio
+            a space where people can come, express, record, mix and above all be creative.A space where technology is a smooth pathway for everything you have in your mind.From chasing the most pristine equipments,to the best space,to the best team,at AuO2,we left no stone unturned to create something that you can lose yourself in."            <br><br>
             -
             <b>Sreejesh Nair</b>
         </p>
