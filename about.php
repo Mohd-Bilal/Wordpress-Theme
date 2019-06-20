a8f1f19967fed9556909a0fe33179e1bb0e9a73c
albin <albinpkr96@gmail.com>
2019-06-20 19:32:54 +0530

    @media changes

--- about.php
+++ about.php
@@ -3,17 +3,20 @@
         <h1>About</h1>
 
         <div class="col-lg-5 col-md-5 col-sm-12 col-xm-12 container  justify-content-right ">
-            <img  src=<?php  echo get_template_directory_uri().'/images/Sreejesh.png' ?>  alt="">
+            <img src=<?php  echo get_template_directory_uri().'/images/Sreejesh.png' ?> alt="">
         </div>
         <div class="col-lg-7 col-md-7 col-sm-12 col-xm-12 ">
-        <p>" A  studio has always been my wish. But a community has always bee my dream. I wanted to make AuO2 Studio
-            a space where people can come, express, record, mix and above all be creative.A space where technology is a smooth pathway for everything you have in your mind.From chasing the most pristine equipments,to the best space,to the best team,at AuO2,we left no stone unturned to create something that you can lose yourself in."            <br><br>
-            -
-            <b>Sreejesh Nair</b>
-        </p>
+            <p>" A studio has always been my wish. But a community has always bee my dream. I wanted to make AuO2 Studio
+                a space where people can come, express, record, mix and above all be creative.A space where technology
+                is a smooth pathway for everything you have in your mind.From chasing the most pristine equipments,to
+                the best space,to the best team,at AuO2,we left no stone unturned to create something that you can lose
+                yourself in." <br><br>
+                -
+                <b>Sreejesh Nair</b>
+            </p>
 
         </div>
-       
+
 
     </div>
 </div>
