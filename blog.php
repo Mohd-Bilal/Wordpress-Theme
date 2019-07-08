78debd11b694b7f0ddd759ba048163821829af5c
Mohd-Bilal <bilaljaleel123@gmail.com>
2019-07-08 22:19:55 +0530

    Created blog part responsiveness unsure

--- blog.php
+++ blog.php
@@ -1,4 +1,10 @@
-<h2 class="facility-head" id="facility">Blog</h2>
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<h2 class="blog_post_title" style="text-align: center;"id="facility">Blog</h2>
+<p class="blog_description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+</div>
+</div>
 <?php
 $rss = fetch_feed( 'https://film-mixing.com/feed' );
 
@@ -14,19 +20,29 @@ if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
 
 endif;
 ?>
-
-<ul>
+<div class="row">
     <?php if ( $maxitems == 0 ) : ?>
         <li><?php _e( 'No items', 'my-text-domain' ); ?></li>
     <?php else : ?>
         <?php // Loop through each feed item and display each item as a hyperlink. ?>
         <?php foreach ( $rss_items as $item ) : ?>
-            <li>
-                <a href="<?php echo esc_url( $item->get_permalink() ); ?>"
-                    title="<?php printf( __( 'Posted %s', 'my-text-domain' ), $item->get_date('j F Y | g:i a') ); ?>">
-                    <?php echo esc_html( $item->get_title() ); ?>
-                </a>
-            </li>
+            <div class="col-md-4" style="text-align:center;">
+            <div class="card blog_card" >
+              <div class="card-body blog_card_body">
+                <h5 class="card-title blog_card_title" > <?php echo esc_html( $item->get_title() ); ?></h5>
+                <h6 class="card-subtitle mb-2 text-muted" ><?php printf( __( '%s', 'my-text-domain' ), $item->get_date('l,F Y '));?></h6>
+                <br>
+                <br>
+                <a href="<?php echo esc_url( $item->get_permalink() ); ?>" class="card-link blog_card_link">Read More ></a>
+              </div>
+            </div>
+            </div>
+
         <?php endforeach; ?>
     <?php endif; ?>
-</ul>
+</div >
+<div style="text-align:center;margin-top: 10%; ">
+    <p style="color:white;">Like to read more articles ?</p>
+    <a href="https://film-mixing.com" class="blog_card_link">Visit the blog now</a>
+</div>
+</div>
