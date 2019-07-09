3d71afdfbecea130e2ee9e3828090410c1373b76
albin <albinpkr96@gmail.com>
2019-07-09 22:48:33 +0530

    facility

--- facility.php
+++ facility.php
@@ -1,63 +1,47 @@
 <hr>
 <h2 class="facility-head" id="facility">Facility</h2>
-<div class="bd-example ">
-    <div id="facilityCarousel" class="carousel slide" style="margin: 0 auto;" data-ride="carousel">
-        <ol class="carousel-indicators" style="bottom:5wh">
-            <li data-target="#facilityCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#facilityCarousel" data-slide-to="1"></li>
-            <li data-target="#facilityCarousel" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner facility-carousel-item">
-            <?php
-            $args = array('post_type'=>'facility','post_status'=>'publish','posts_per_page'=>3);
-            $loop = new WP_Query($args);
-            if($loop->have_posts()):
-                $flag = True;
-            while($loop->have_posts()) : $loop->the_post(); ?>
-                    <?php 
-            if($flag) :
-                echo '<div class="carousel-item active facility_carousel ">';
-            else :
-                echo '<div class="carousel-item facility_carousel">';
-            endif;
-            $flag = False;
-            ?>
-            <div class="facility-description row align-items-center">
-                <div class="col-md-4 align-self-center">
-                    <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>"
-                        class="img-fluid d-block rounded-circle facility_carousel_image " alt="...">
-                </div>
-                <div class="col-md-8 align-self-center">
-                    <h2><?php the_title() ?></h2>
-                    <br>
-                    <p><?php the_excerpt() ?></p>
-                </div>
+
+<div class="row card-deck" style="margin:0px;padding:0px">
+    <!-- <div class="card-deck"> -->
+    <?php
+        $args = array('post_type'=>'facility','post_status'=>'publish','posts_per_page'=>3);
+        $loop = new WP_Query($args);
+        if($loop->have_posts()):
+        while($loop->have_posts()) : $loop->the_post(); 
+    ?>
+
+    <!-- <div class="row">
+        <div class="col-md-4">
+            <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>">
+        </div>
+        <h2><?php the_title() ?></h2>
+        <br>
+        <p><?php the_excerpt() ?></p>
+    </div> -->
+
+    <div class="col-md-4 d-flex align-items-stretch">
+        <div class="card mb-3">
+            <img class="card-img-top facility_carousel_image" src="<?php echo get_the_post_thumbnail_url(get_the_ID(),'full'); ?>"
+                alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title"><?php the_title() ?></h5>
+                <p class="card-text"><?php the_excerpt() ?></p>
             </div>
         </div>
+    </div>
 
-        <?php endwhile;
- 
+
+
+    <?php endwhile;
         else :
             echo '<p>There are no posts!</p>';
-    
         endif;
+    ?>
+    <!-- </div> -->
 
-        ?>
 
-    </div>
-
-    <div style="bottom : 1px">
-        <a class="carousel-control-prev" href="#facilityCarousel" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#facilityCarousel" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
-</div>
 
 
 </div>
-<p class="facility_vip">In addition to this,AuO2 Studio also has a private VIP lounge for the discussions or rest you might want to have in between that creative action along with high speed internet access</p>
+<p class="facility_vip">In addition to this,AuO2 Studio also has a private VIP lounge for the discussions or rest you
+    might want to have in between that creative action along with high speed internet access</p>
