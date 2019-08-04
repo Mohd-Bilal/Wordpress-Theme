742c119ca7eac2f079fa6d1b97bfc46219dade40
albin <albinpkr96@gmail.com>
2019-08-04 09:03:35 +0530

    button outline removed

--- gallery.php
+++ gallery.php
@@ -1,4 +1,4 @@
-<h2 class="facility-head" id="facility">Gallery</h2>
+ <h2 class="facility-head" id="facility">Gallery</h2>
 <div class="container">
 	<div class="row">
         <div class="col-md-12">
