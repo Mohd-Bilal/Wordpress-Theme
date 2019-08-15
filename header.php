3bdb80dfa0004716484222241dad57681fba820d
Mohd-Bilal <bilaljaleel123@gmail.com>
2019-08-15 18:23:26 +0530

    fixes

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
     <img class="studio-logo navbar-brand order-md-last" src=<?php echo get_template_directory_uri().'/images/Image2.png'?> >
     
     <div class="navbar-collapse navbar-left collapse"  id="navbarText" >
-        <ul  class="navbar-nav mr-auto  ">
+            <ul  class="navbar-nav mr-auto  ">
             <li class="nav-item active">
                 <a class="nav-link" href="#about">ABOUT <span class="sr-only">(current)</span></a>
             </li>
