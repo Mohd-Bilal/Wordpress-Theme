9cb7ce43def944c38d32fa20af1f6267a21b8ce7
Mohd-Bilal <bilaljaleel123@gmail.com>
2019-08-26 22:10:33 +0530

    About description change

--- about.php
+++ about.php
@@ -6,11 +6,8 @@
             <img src=<?php  echo get_template_directory_uri().'/images/Sreejesh.png' ?> alt="">
         </div>
         <div class="col-lg-7 col-md-7 col-sm-12 col-xm-12 ">
-            <p>" A studio has always been my wish. But a community has always bee my dream. I wanted to make AuO2 Studio
-                a space where people can come, express, record, mix and above all be creative.A space where technology
-                is a smooth pathway for everything you have in your mind.From chasing the most pristine equipments,to
-                the best space,to the best team,at AuO2,we left no stone unturned to create something that you can lose
-                yourself in." <br><br>
+            <p>"It's always been my dream to have a studio of my own one day -
+a haven for professional and budding artists alike, a place where passionate souls can fully express themselves, learn and grow as creators in their own right and make magic, day in and out. A space that contains within itself the perfect blend of artistry and technology, which helps you craft a reality as sublime as the vision in your mind. To which end we’ve furnished the place with the latest equipment and hired the best team to be of assistance to you. We keep an open house - for every freelancer and engineer - not only for making music, but also ADR and voiceovers. And in the years to come, it is my firm belief that this studio and all those who lovingly come to be associated with it, will leave an everlasting impact on the recording industry." <br><br>
                 -
                 <b>Sreejesh Nair</b>
             </p>
