0d4c2e700e1a1fedfeee697668d9e6ddb629ce84
albin <albinpkr96@gmail.com>
2019-09-13 12:02:27 +0530

    filmography

--- functions.php
+++ functions.php
@@ -197,22 +197,121 @@ function team_carousel_items(){
 }
 add_action('init','team_carousel_items'); 
 
-function filmography_items(){
-    register_post_type('Films',
-    array(
-        'rewrite' => array('slug'=>'filmography_items'),
-        'labels' => array(
-            'name' => 'Films',
-            'singular_name' => 'Film',
-            'add_new_item' => 'Add New Film',
-            'edit_item' => 'Edit Film'
-        ),
-        'menu-icon' => 'dashicons-format-image',
-        'public' => true,
-        'has_archive' => true,
-        'supports' => array(
-            'title','thumbnail','editor'
-        ) 
-    ));
+
+
+
+function filmography_items() {
+	$labels = array(
+		'name'               => __( 'Films' ),
+		'singular_name'      => __( 'Films' ),
+		'add_new'            => __( 'Add New Film' ),
+		'add_new_item'       => __( 'Add New Film' ),
+		'edit_item'          => __( 'Edit Film' ),
+		'new_item'           => __( 'Add New Film' ),
+		'view_item'          => __( 'View Film' ),
+		'search_items'       => __( 'Search Film' ),
+		'not_found'          => __( 'No films found' ),
+		'not_found_in_trash' => __( 'No filmd found in trash' )
+	);
+	$supports = array(
+		'title',
+		'thumbnail',
+		'revisions',
+	);
+	$args = array(
+		'labels'               => $labels,
+		'supports'             => $supports,
+		'public'               => true,
+		'capability_type'      => 'post',
+		'rewrite'              => array( 'slug' => 'films' ),
+		'has_archive'          => true,
+		'menu_position'        => 30,
+		'register_meta_box_cb' => 'wpt_add_film_metaboxes',
+	);
+	register_post_type( 'films', $args );
 }
-add_action('init','filmography_items'); 
+add_action( 'init', 'filmography_items' );
+/**
+ * Adds a metabox to the right side of the screen under the “Publish” box
+ */
+function wpt_add_film_metaboxes() {
+	add_meta_box(
+		'wpt_films_date',
+		'Film Date',
+		'wpt_films_date',
+		'films',
+		'normal',
+		'default'
+	);
+	add_meta_box(
+		'wpt_films_link',
+		'Film Link',
+		'wpt_films_link',
+		'films',
+		'normal',
+		'default'
+	);
+}
+/**
+ * Output the HTML for the metabox.
+ */
+function wpt_films_link() {
+	global $post;
+	// Nonce field to validate form request came from current site
+	wp_nonce_field( basename( __FILE__ ), 'film_fields' );
+	// Get the link data if it's already been entered
+	$link = get_post_meta( $post->ID, 'link', true );
+	// Output the field
+	echo '<input type="text" name="link" value="' . esc_textarea( $link )  . '" class="widefat">';
+}
+
+function wpt_films_date() {
+	global $post;
+	// Nonce field to validate form request came from current site
+	wp_nonce_field( basename( __FILE__ ), 'film_fields' );
+	// Get the link data if it's already been entered
+	$link = get_post_meta( $post->ID, 'date', true );
+	// Output the field
+	echo '<input type="text" name="date" value="' . esc_textarea( $link )  . '" class="widefat">';
+}
+/**
+ * Save the metabox data
+ */
+function wpt_save_films_meta( $post_id, $post ) {
+	// Return if the user doesn't have edit permissions.
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return $post_id;
+	}
+	// Verify this came from the our screen and with proper authorization,
+	// because save_post can be triggered at other times.
+	if ( ! isset( $_POST['date'] ) || ! wp_verify_nonce( $_POST['film_fields'], basename(__FILE__) ) ) {
+		return $post_id;
+    }
+    if ( ! isset( $_POST['link'] ) || ! wp_verify_nonce( $_POST['film_fields'], basename(__FILE__) ) ) {
+		return $post_id;
+	}
+	// Now that we're authenticated, time to save the data.
+	// This sanitizes the data from the field and saves it into an array $films_meta.
+    $films_meta['date'] = esc_textarea( $_POST['date'] );
+    $films_meta['link'] = esc_textarea( $_POST['link'] );
+	// Cycle through the $films_meta array.
+	// Note, in this example we just have one item, but this is helpful if you have multiple.
+	foreach ( $films_meta as $key => $value ) :
+		// Don't store custom data twice
+		if ( 'revision' === $post->post_type ) {
+			return;
+		}
+		if ( get_post_meta( $post_id, $key, false ) ) {
+			// If the custom field already has a value, update it.
+			update_post_meta( $post_id, $key, $value );
+		} else {
+			// If the custom field doesn't have a value, add it.
+			add_post_meta( $post_id, $key, $value);
+		}
+		if ( ! $value ) {
+			// Delete the meta key if there's no value
+			delete_post_meta( $post_id, $key );
+		}
+	endforeach;
+}
+add_action( 'save_post', 'wpt_save_films_meta', 1, 2 );
--- page-filmography.php
+++ page-filmography.php
@@ -1,7 +1,40 @@
 <?php get_header() ?>
-<div class="filmography">
-<li>Hello</li>
-<li>World</li>
-</div>
+<div class="filmography row">
+    <div class="col-md-12">
+        <div class="row">
+            <div class=" col-md-4">
+            <div class="film-circle"><span>Lorem ipsum</span></div>
+            </div>
+            <div class=" col-md-4">
+            <div class="film-circle"><span>Lorem ipsum</span></div>
+            </div>
+            <div class="  col-md-4">
+            <div class="film-circle"><span>Lorem ipsum</span></div>
+            </div>
+        </div>
 
+    </div>
+
+
+    <?php
+        $args = array('post_type'=>'films','post_status'=>'publish','posts_per_page'=>-1);
+        $loop = new WP_Query($args);
+        if($loop->have_posts()):
+        while($loop->have_posts()) : $loop->the_post(); 
+        $dates = get_post_custom_values( 'date' );
+        $links = get_post_custom_values( 'link' );
+    ?>
+
+    
+
+    <div class="films col-md-6">
+    <p><a href="<?php echo $links[0]?>"><?php the_title() ?> </a>&nbsp&nbsp<?php echo $dates[0] ?></p>
+        </div>
+
+    <?php endwhile;
+        else :
+            echo '<p>There are no posts!</p>';
+        endif;
+    ?>
+</div>
 <?php get_footer() ?>
